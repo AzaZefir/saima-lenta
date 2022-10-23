335d9710aff9d4354cedf0f7097a223d6663f593
azat <arykov.azat.kg@gmail.com>
2022-10-23 10:20:17 +0600

    create quize, result,scss components

---
+++ src/components/quize/Quize.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Quize = () => {
+  return (
+    <div>
+      <div className="progress">
+        <div style={{width:'30%'}} className="progress__bar"></div>
+      </div>
+      <h1>Что такое HTML?</h1>
+      <ul>
+        <li>Это разметка</li>
+        <li>Это разметка</li>
+        <li>Это разметка</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Quize;
---
+++ src/components/result/Result.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+        <img src="https://cdn0.iconfinder.com/data/icons/pug/512/pug_dog_sticker_emoji_emoticon_hacker-512.png" alt="" />
+        <h1>Правильно 3 из 3</h1>
+        <button>Начать с начала</button>
+    </div>
+  )
+}
+
+export default Result
