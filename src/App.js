ef280018f1db3dec641bc2c955c6db00e10eb332
azat <arykov.azat.kg@gmail.com>
2023-12-21 18:59:51 +0600

    clear unused files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from "./components/main/Main";
 function App() {
   
   return (
-    <div className="App">
+    <div className="container">
       <Header/>
       <Main/>
       <Footer/>
