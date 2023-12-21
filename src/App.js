dcab09d449acc98bcaf4282f992b7e931a2baeea
azat <arykov.azat.kg@gmail.com>
2023-12-21 18:30:57 +0600

    clear unused files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import "./App.css";
-import Quize from "./components/quize/Quize";
-import Result from "./components/result/Result";
-import { useState } from "react";
-import { questions } from "./db/db";
 import { Header } from "./components/header/Header";
 
 function App() {
-  const [step, setStep] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const question = questions[step];
-
-  const onClickVariants = (index) => {
-    setStep(step + 1);
-    if (index === question.correct) {
-      setCorrect(correct + 1);
-    }
-  };
   
   return (
     <div className="App">
-      {/* <Header/> */}
-      {step !== questions.length ? (
-        <Quize
-          step={step}
-          question={question}
-          onClickVariants={onClickVariants}
-        />
-      ) : (
-        <Result correct={correct}/>
-      )}
+      <Header/>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { questions } from './../../db/db';
 
 export const Header = () => {
   return (
     <div>
-      {questions.length}
+header
     </div>
   )
 }
